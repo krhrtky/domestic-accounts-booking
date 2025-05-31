65b23abb5d735f4e56d8808d559569f7ea3add67
takuya.kurihara <takuya.kurihara00@gmail.com>
2025-05-31 18:57:11 +0900

    update: change lost-pixel config.

--- frontend/lostpixel.config.ts
+++ frontend/lostpixel.config.ts
@@ -5,6 +5,7 @@ export const config: CustomProjectConfig = {
     ladleUrl: "http://172.17.0.1:61000",
   },
   generateOnly: true,
+  failOnDifference: true,
   lostPixelProjectId: process.env.LOST_PIXEL_PROJECT_ID,
   apiKey: process.env.LOST_PIXEL_API_KEY,
 };
