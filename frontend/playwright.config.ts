eb55cf5cb0c00e2f45a6c474740d8eb3c893151f
takuya.kurihara <takuya.kurihara00@gmail.com>
2025-06-02 08:30:49 +0900

    test: setup visual regression test

--- frontend/playwright.config.ts
+++ frontend/playwright.config.ts
@@ -57,16 +57,16 @@ export default defineConfig({
       use: { ...devices['Desktop Chrome'] },
     },
 
-    {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
-    },
-
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
-    },
-
+    // {
+    //   name: 'firefox',
+    //   use: { ...devices['Desktop Firefox'] },
+    // },
+    //
+    // {
+    //   name: 'webkit',
+    //   use: { ...devices['Desktop Safari'] },
+    // },
+    //
     /* Test against mobile viewports. */
     // {
     //   name: 'Mobile Chrome',
@@ -89,9 +89,9 @@ export default defineConfig({
   ],
 
   /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://localhost:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
+  webServer: {
+    command: 'npm run ladle',
+    url: 'http://localhost:61000',
+    reuseExistingServer: !process.env.CI,
+  },
 });
